97cd105280b40a852d10994fd3601bd8aac78673
asyraf07 <asyraf.sr07@gmail.com>
2024-07-12 13:20:36 +0700

    add keymap for ordering number

--- nvim/lua/asyraf07/remap.lua
+++ nvim/lua/asyraf07/remap.lua
@@ -1,3 +1,4 @@
+-- Netrw keymap
 -- vim.keymap.set("n", "<leader>ph", vim.cmd.Ex)
 
 vim.keymap.set("n", "<leader><leader>", ":so<CR>")
@@ -5,3 +6,5 @@ vim.keymap.set("n", "<leader><leader>", ":so<CR>")
 vim.keymap.set("n", "s", "<C-w>")
 
 vim.keymap.set("n", "<F3>", ":set hlsearch!<CR>")
+
+vim.keymap.set("n", "<S-s>", "VG:s/\\w*\\. /0. /<CR><C-o>VGg<C-a>:noh<CR>")
